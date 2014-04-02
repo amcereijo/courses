9489e9f56dfd386bf1bff7bde478abfc5fec31ef
amcereijo <amcereijo@gmail.com>
2014-04-02 21:29:39 +0200

    controller de test como mensaje por defecto para la app

--- courses-spring/src/main/java/com/angel/courses/controller/TestController.java
+++ courses-spring/src/main/java/com/angel/courses/controller/TestController.java
@@ -1,22 +1,23 @@
 package com.angel.courses.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.angel.courses.repository.CourseRepository;
+
+
 @Controller
-@EnableAutoConfiguration
 public class TestController {
 
+	@Autowired
+	private CourseRepository courseRepository;
+	
 	@RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello Courses!";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(TestController.class, args);
-    }
 }
