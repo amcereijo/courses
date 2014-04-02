0b95ff8ae2ec90c1d4e31da68aa2cdd8f2fd1d43
amcereijo <amcereijo@gmail.com>
2014-04-02 21:11:18 +0200

    test de api para niveles

---
+++ courses-spring/src/test/java/com/angel/courses/api/LevelsApiTest.java
@@ -0,0 +1,43 @@
+package com.angel.courses.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.angel.courses.Application;
+import com.angel.courses.config.DatabaseConfiguration;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {Application.class, DatabaseConfiguration.class})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, 
+	DbUnitTestExecutionListener.class })
+@DatabaseSetup("/data/levelsData.xml")
+@DatabaseTearDown(type=DatabaseOperation.DELETE_ALL, value="/data/levelsData.xml")
+public class LevelsApiTest extends BaseApitTest{
+
+	@Test
+	public void testGet() throws Exception{
+		mockMvc.perform(
+			get("/api/levels")
+				.contentType(MediaType.APPLICATION_JSON)
+				)
+			.andExpect(status().isOk())
+			.andExpect(content().contentType("application/json"))
+			.andReturn();
+	}
+}
