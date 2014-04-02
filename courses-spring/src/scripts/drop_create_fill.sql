fab37de63f3ffbd94bc3a5559bee2bc7ba0e6596
amcereijo <amcereijo@gmail.com>
2014-04-02 19:08:20 +0200

    corrección para crear tablas y datos

--- courses-spring/src/scripts/drop_create_fill.sql
+++ courses-spring/src/scripts/drop_create_fill.sql
@@ -1,10 +1,10 @@
 drop table courses.courses;
-drop table courses.authors;
+drop table courses.teachers;
 drop table courses.levels;
 
 create table courses.levels(id bigint auto_increment 	primary key, name varchar(50));
 
-create table courses.authors (
+create table courses.teachers (
 	id bigint auto_increment primary key,
 	name varchar(100)
 );
@@ -15,9 +15,9 @@ create table courses.courses (
 	id_level bigint,
 	hours int,
 	active boolean,
-	id_author bigint,
-	FOREIGN KEY (id_author) 
-        REFERENCES courses.authors(id)
+	id_teacher bigint,
+	FOREIGN KEY (id_teacher) 
+        REFERENCES courses.teachers(id)
         ON DELETE CASCADE,
     FOREIGN KEY (id_level) 
         REFERENCES courses.levels(id)
@@ -27,19 +27,19 @@ create table courses.courses (
 
 
 
-insert into courses.authors(id, name) values(1,'Angel Cereijo Martinez');
-insert into courses.authors(id, name) values(2,'Steve Jobs');
-insert into courses.authors(id, name) values(3,'Will Smith');
-insert into courses.authors(id, name) values(4,'Bruce Willies');
+insert into courses.teachers(id, name) values(1,'Angel Cereijo Martinez');
+insert into courses.teachers(id, name) values(2,'Steve Jobs');
+insert into courses.teachers(id, name) values(3,'Will Smith');
+insert into courses.teachers(id, name) values(4,'Bruce Willies');
 
 
 insert into courses.levels(id, name) values(1 ,'Básico');
 insert into courses.levels(id, name) values(2 ,'Intermedio');
 insert into courses.levels(id, name) values(3 ,'Avanzado');
 
-insert into courses.courses(id, title, id_level, hours, active, id_author) 
+insert into courses.courses(id, title, id_level, hours, active, id_teacher) 
 					values ( 1, 'My first course', 1,10, 0, 1);
-insert into courses.courses(id, title, id_level, hours, active, id_author) 
+insert into courses.courses(id, title, id_level, hours, active, id_teacher) 
 					values ( 2, 'Never ending course', 2, 25, 0, 1);
-insert into courses.courses(id, title, id_level, hours, active, id_author) 
+insert into courses.courses(id, title, id_level, hours, active, id_teacher) 
 					values ( 3, 'Expert course', 3, 100, 0, 3);
