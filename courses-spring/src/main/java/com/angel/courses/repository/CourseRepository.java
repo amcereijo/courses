744c5debe442190645b3790dae3574cf84543a9b
amcereijo <amcereijo@gmail.com>
2014-04-02 19:08:35 +0200

    competar repository para courses y test

--- courses-spring/src/main/java/com/angel/courses/repository/CourseRepository.java
+++ courses-spring/src/main/java/com/angel/courses/repository/CourseRepository.java
@@ -1,5 +1,7 @@
 package com.angel.courses.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import com.angel.courses.domain.Course;
@@ -8,4 +10,8 @@ public interface CourseRepository {
 
 //	@Select("SELECT * FROM courses WHERE id = #{id}")
 	Course getCourseById(@Param("id") Long id);
+	
+	List<Course> getAll();
+	
+	Long insert(Course course);
 }
---
+++ courses-spring/src/test/java/com/angel/courses/repository/CourseRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.angel.courses.repository;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import com.angel.courses.config.DatabaseConfiguration;
+import com.angel.courses.domain.Course;
+import com.angel.courses.domain.Level;
+import com.angel.courses.domain.Teacher;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DatabaseConfiguration.class})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, 
+	DbUnitTestExecutionListener.class })
+
+@DatabaseSetup("/data/coursesData.xml")
+@DatabaseTearDown(type=DatabaseOperation.DELETE_ALL, value="/data/coursesData.xml")
+public class CourseRepositoryTest {
+
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	
+	@Test
+	public void test(){
+		List<Course> courses = courseRepository.getAll();
+		Assert.assertTrue(courses.size() == 3);
+		System.out.println("Test");
+	}
+	
+	@Test
+	public void testInsert(){
+		Course course = new Course()
+			.setActive(Boolean.TRUE)
+			.setHours(100)
+			.setLevel(new Level().setId(1L))
+			.setTeacher(new Teacher().setId(1L))
+			.setTitle("Title");
+		
+		Long id = courseRepository.insert(course);
+		
+		Assert.assertNotNull(course);
+		Assert.assertNotNull(id);
+		
+	}
+}
