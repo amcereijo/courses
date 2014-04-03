cffd2df1ee7dce87e42c924d0bb7eddaa9e23cbb
amcereijo <amcereijo@gmail.com>
2014-04-03 15:29:01 +0200

    test

--- courses-angular/test/unit/controllersSpec.js
+++ courses-angular/test/unit/controllersSpec.js
@@ -3,17 +3,69 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
+
+  beforeEach(function(){
+    this.addMatchers({
+      toEqualData: function(expected) {
+        return angular.equals(this.actual, expected);
+      }
+    });
+  });
+
+  beforeEach(module('courses'));
   beforeEach(module('courses.controllers'));
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('CourseListController');
-    expect(myCtrl1).toBeDefined();
-  }));
+  describe('CourseListController', function(){
+    //$scope','$http','$rootScope'
+    var scope, $http, rootScope,ctrl;
+  
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+     $http = _$httpBackend_;
+     $http.expectGET('/courses').
+          respond(
+            [
+              {"id":1,"title":"My first course","level":{"id":1,"name":"Básico"},
+                "hours":10,"active":false,"teacher":{"id":1,"name":"Básico"}
+              },
+              {"id":2,"title":"Never ending course","level":{"id":2,"name":"Básico"},
+                "hours":25,"active":false,"teacher":{"id":2,"name":"Básico"}
+              },
+              {"id":3,"title":"Expert course","level":{"id":3,"name":"Básico"},
+                "hours":100,"active":false,"teacher":{"id":3,"name":"Básico"}
+              }
+            ]);
+      rootScope = $rootScope;
+      rootScope.serviceUrl = 'http://localhost:9999/api';
+      scope = $rootScope.$new();
+      ctrl = $controller('CourseListController', {$scope: scope});
+    }));
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('CourseNewController');
-    expect(myCtrl2).toBeDefined();
-  }));
+    it('should ....', inject(function($controller) {
+      //spec body
+      /*var myCtrl1 = $controller('CourseListController',
+        [scope, $http, rootScope]);
+      expect(myCtrl1).toBeDefined();*/
+      expect(ctrl).toBeDefined();
+
+    }));
+  });
+  
+
+  describe('CourseNewController', function(){
+    var scope, $http, rootScope,ctrl;
+  
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+     $http = _$httpBackend_;
+      rootScope = $rootScope;
+      rootScope.serviceUrl = 'http://localhost:9999/api';
+      scope = $rootScope.$new();
+      ctrl = $controller('CourseNewController', {$scope: scope});
+    }));
+
+    it('should ....', inject(function($controller) {
+      expect(ctrl).toBeDefined();
+    }));
+  });
+/*
+  */
 });
--- courses-angular/test/unit/filtersSpec.js
+++ courses-angular/test/unit/filtersSpec.js
@@ -16,4 +16,53 @@ describe('filter', function() {
       expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
     }));
   });
+
+  describe('startFrom', function(){
+  	it('should move start value 1 to 5', inject(function(startFromFilter){
+  		var courses = [
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'}
+  		],
+  		coursesPaged = [
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'},
+  			{'id':1,'title':'title1'}
+  		],
+  		start = 1,
+  		pageSize = 5;
+  		expect(startFromFilter(courses, (start*pageSize))).toEqual(coursesPaged);
+  	}));
+  });
+
+  describe('showOrderBy', function(){
+  	it('should return glyphicon glyphicon-sort-by-attributes-alt', inject(function(showOrderByFilter){
+  		var expresion = true,
+  			orderbyASC = true;
+  			expect(showOrderByFilter(expresion, orderbyASC)).toEqual('glyphicon glyphicon-sort-by-attributes-alt');
+  	}));
+  });
+  describe('showOrderBy', function(){
+  	it('should return glyphicon glyphicon-sort-by-attributes', inject(function(showOrderByFilter){
+  		var expresion = true,
+  			orderbyASC = false;
+  			expect(showOrderByFilter(expresion, orderbyASC)).toEqual('glyphicon glyphicon-sort-by-attributes');
+  	}));
+  });
+  describe('showOrderBy', function(){
+  	it('should return ""', inject(function(showOrderByFilter){
+  		var expresion = false,
+  			orderbyASC = false;
+  			expect(showOrderByFilter(expresion, orderbyASC)).toEqual('');
+  	}));
+  });
 });
